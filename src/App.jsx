e6d415ee587b9d1145960a4ebb9e552d9ca8bcf1
matheus albuquer <mj898926@gmail.com>
2026-06-16 21:55:22 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 
-import Projects from "./components/Projects/projects";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
